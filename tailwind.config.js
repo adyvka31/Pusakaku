23c5e67c64bd725efa63ee890060b2f1858b35a3
Bdullsss <abdaja.9@gmail.com>
2025-01-15 16:55:49 +0700

    responsive 1.0.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'xs': '250px',
+
+      'sm': '420px',
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+
     extend: {},
   },
   plugins: [],
